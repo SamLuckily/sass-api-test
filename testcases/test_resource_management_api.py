be82d697c7c7b75ab4d3d57be4c96dffb23d3743
15830344885 <15830344885@163.com>
2024-05-29 10:54:30 +0800

    jsonpath方法封装用于断言提取数据

--- testcases/test_resource_management_api.py
+++ testcases/test_resource_management_api.py
@@ -6,6 +6,7 @@ from api.CourseManagementApi import CourseManagementApi
 from api.LessonManagementApi import LessonManagementApi
 from api.ResourceManagementApi import ResourceManagementApi
 from api.UniversalApi import UniversalApi
+from utils.jsonpath_utils import JsonPathUtils
 
 
 class TestResourceManagementApi(BaseApi):
@@ -24,15 +25,18 @@ class TestResourceManagementApi(BaseApi):
     def test_resource_binding(self):
         """资源绑定"""
         r = self.course_management.course_add("测试课程", self.access_token()[1], "publish", "测试课程")
-        course_uuid = jsonpath.jsonpath(r, "$..uuid")[0]
+        course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
         r = self.chapter_management.chapter_add("测试章节", course_uuid)
-        chapter_uuid = jsonpath.jsonpath(r, "$..uuid")[0]
+        chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
         r = self.lessonManagement.lesson_add("测试课时", course_uuid, chapter_uuid, "1")
-        lesson_uuid = jsonpath.jsonpath(r, "$..uuid")[0]
+        lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
         r = self.universal.upload_resource()
-        file_uuid = jsonpath.jsonpath(r, "$..uuid")[0]
+        file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
         self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
         self.course_management.course_delete(course_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        assert r.get("code") == 0
+        assert code
 
     def test_resource_unbinding(self):
         assert False
---
+++ utils/jsonpath_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import jsonpath
+
+from utils.log_util import logger
+
+
+class JsonPathUtils:
+    @classmethod
+    def get(cls, obj, expr):
+        res = jsonpath.jsonpath(obj, expr)
+        logger.info(f"通过jsonpath获取到的数据为{res}")
+        return res
