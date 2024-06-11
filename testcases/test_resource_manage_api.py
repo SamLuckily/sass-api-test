0b0d734681023d1c234a4262c1336e16ac982cf8
15830344885 <15830344885@163.com>
2024-06-11 17:25:32 +0800

    预发布环境

--- testcases/test_resource_manage_api.py
+++ testcases/test_resource_manage_api.py
@@ -58,123 +58,123 @@ class TestResourceManagementApi(BaseApi):
     #     logger.info(f"验证的结果为：{res}")
     #     assert res
 
-    # @allure.story("资源绑定测试用例")
-    # @allure.title("资源绑定")
-    # @allure.severity('normal')
-    # @allure.description("资源绑定")
-    # @pytest.mark.parametrize("data", get_data()['resource_binding'])
-    # def test_resource_binding(self, data):
-    #     r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
-    #                                           data["about"])
-    #     course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
-    #     chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
-    #     lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.universal.upload_resource()
-    #     file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
-    #     self.course_management.course_delete(course_uuid)
-    #     code = JsonPathUtils.get(r, "$..code")
-    #     # 断言
-    #     assert r.get("code") == 0
-    #     assert len(code) == 1 and code[0] == 0
-    #
-    # @allure.story("资源解除绑定测试用例")
-    # @allure.title("资源解除绑定")
-    # @allure.severity('normal')
-    # @allure.description("资源解除绑定")
-    # @pytest.mark.parametrize("data", get_data()['resource_unbinding'])
-    # def test_resource_unbinding(self, data):
-    #     r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
-    #                                           data["about"])
-    #     course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
-    #     chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
-    #     lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.universal.upload_resource()
-    #     file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
-    #     self.resource_management.resource_unbinding("course_lesson", lesson_uuid, file_uuid)
-    #     self.course_management.course_delete(course_uuid)
-    #     code = JsonPathUtils.get(r, "$..code")
-    #     # 断言
-    #     assert r.get("code") == 0
-    #     assert len(code) == 1 and code[0] == 0
-    #
-    # @allure.story("资源编辑测试用例")
-    # @allure.title("资源编辑")
-    # @allure.severity('normal')
-    # @allure.description("资源编辑")
-    # @pytest.mark.parametrize("data", get_data()['resource_edit'])
-    # def test_resource_edit(self, data):
-    #     """资源编辑"""
-    #     r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
-    #                                           data["about"])
-    #     course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
-    #     chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
-    #     lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.universal.upload_resource()
-    #     file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
-    #     self.resource_management.resource_edit(data['file_name'], file_uuid)
-    #     self.course_management.course_delete(course_uuid)
-    #     code = JsonPathUtils.get(r, "$..code")
-    #     # 断言
-    #     assert r.get("code") == 0
-    #     assert len(code) == 1 and code[0] == 0
-    #
-    # @allure.story("资源删除测试用例")
-    # @allure.title("资源删除")
-    # @allure.severity('normal')
-    # @allure.description("资源删除")
-    # @pytest.mark.parametrize("data", get_data()['resource_delete'])
-    # def test_resource_delete(self, data):
-    #     r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
-    #                                           data["about"])
-    #     course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
-    #     chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
-    #     lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.universal.upload_resource()
-    #     file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
-    #     self.resource_management.resource_unbinding("course_lesson", lesson_uuid, file_uuid)
-    #     self.resource_management.resource_delete(file_uuid)
-    #     self.course_management.course_delete(course_uuid)
-    #     code = JsonPathUtils.get(r, "$..code")
-    #     # 断言
-    #     assert r.get("code") == 0
-    #     assert len(code) == 1 and code[0] == 0
-    #
-    # @allure.story("资源排序测试用例")
-    # @allure.title("资源排序")
-    # @allure.severity('normal')
-    # @allure.description("资源排序")
-    # @pytest.mark.parametrize("data", get_data()['resource_sort'])
-    # def test_resource_sorting(self, data):
-    #     r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
-    #                                           data["about"])
-    #     course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
-    #     chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
-    #     lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     r = self.universal.upload_resource()
-    #     file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
-    #     self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
-    #     r = self.universal.upload_resource()
-    #     file_uuids = JsonPathUtils.get(r, "$..uuid")[0]
-    #     self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuids)
-    #     r = self.course_management.course_details(course_uuid)
-    #     belong_id = JsonPathUtils.get(r, "$..resource[0].belong_id")[0]
-    #     self.resource_management.resource_sorting(file_uuid, belong_id, data['sort'])
-    #     self.course_management.course_delete(course_uuid)
-    #     code = JsonPathUtils.get(r, "$..code")
-    #     # 断言
-    #     assert r.get("code") == 0
-    #     assert len(code) == 1 and code[0] == 0
+    @allure.story("资源绑定测试用例")
+    @allure.title("资源绑定")
+    @allure.severity('normal')
+    @allure.description("资源绑定")
+    @pytest.mark.parametrize("data", get_data()['resource_binding'])
+    def test_resource_binding(self, data):
+        r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
+                                              data["about"])
+        course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
+        chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
+        lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.universal.upload_resource()
+        file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
+        self.course_management.course_delete(course_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("资源解除绑定测试用例")
+    @allure.title("资源解除绑定")
+    @allure.severity('normal')
+    @allure.description("资源解除绑定")
+    @pytest.mark.parametrize("data", get_data()['resource_unbinding'])
+    def test_resource_unbinding(self, data):
+        r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
+                                              data["about"])
+        course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
+        chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
+        lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.universal.upload_resource()
+        file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
+        self.resource_management.resource_unbinding("course_lesson", lesson_uuid, file_uuid)
+        self.course_management.course_delete(course_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("资源编辑测试用例")
+    @allure.title("资源编辑")
+    @allure.severity('normal')
+    @allure.description("资源编辑")
+    @pytest.mark.parametrize("data", get_data()['resource_edit'])
+    def test_resource_edit(self, data):
+        """资源编辑"""
+        r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
+                                              data["about"])
+        course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
+        chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
+        lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.universal.upload_resource()
+        file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
+        self.resource_management.resource_edit(data['file_name'], file_uuid)
+        self.course_management.course_delete(course_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("资源删除测试用例")
+    @allure.title("资源删除")
+    @allure.severity('normal')
+    @allure.description("资源删除")
+    @pytest.mark.parametrize("data", get_data()['resource_delete'])
+    def test_resource_delete(self, data):
+        r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
+                                              data["about"])
+        course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
+        chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
+        lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.universal.upload_resource()
+        file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
+        self.resource_management.resource_unbinding("course_lesson", lesson_uuid, file_uuid)
+        self.resource_management.resource_delete(file_uuid)
+        self.course_management.course_delete(course_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("资源排序测试用例")
+    @allure.title("资源排序")
+    @allure.severity('normal')
+    @allure.description("资源排序")
+    @pytest.mark.parametrize("data", get_data()['resource_sort'])
+    def test_resource_sorting(self, data):
+        r = self.course_management.course_add(data['class_name'], self.access_token()[1], data['publish'],
+                                              data["about"])
+        course_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.chapter_management.chapter_add(data["name_chapter"], course_uuid)
+        chapter_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.lessonManagement.lesson_add(data['name_lesson'], course_uuid, chapter_uuid, data['view'])
+        lesson_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        r = self.universal.upload_resource()
+        file_uuid = JsonPathUtils.get(r, "$..uuid")[0]
+        self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuid)
+        r = self.universal.upload_resource()
+        file_uuids = JsonPathUtils.get(r, "$..uuid")[0]
+        self.resource_management.resource_binding("course_lesson", lesson_uuid, file_uuids)
+        r = self.course_management.course_details(course_uuid)
+        belong_id = JsonPathUtils.get(r, "$..resource[0].belong_id")[0]
+        self.resource_management.resource_sorting(file_uuid, belong_id, data['sort'])
+        self.course_management.course_delete(course_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
