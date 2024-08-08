eca5f929a0d001f2970a27c252e31bfccf89162d
15830344885 <15830344885@163.com>
2024-08-08 17:24:15 +0800

    用户管理全部接口

--- api/UserManageApi.py
+++ api/UserManageApi.py
@@ -13,5 +13,126 @@ class UserManageApi(BaseApi):
             "page": page,
             "size": size
         }
-        path = "/backend/user/list"
+        path = "backend/user/list"
         return self.send("get", path, params=data)
+
+    def get_user_list_null(self):
+        """获取用户列表不传递参数"""
+        path = "backend/user/list"
+        return self.send("get", path)
+
+    def add_student(self, phone, name, pwd):
+        """添加学生"""
+        data = {
+            "phone": phone,
+            "name": name,
+            "pwd": pwd
+        }
+        path = "backend/user/add-student"
+        return self.send("post", path, json=data)
+
+    def del_user(self, user_uuid):
+        """删除用户"""
+        data = {
+            "user_uuid": user_uuid
+        }
+        path = "backend/v2/user/del"
+        return self.send("post", path, json=data)
+
+    def add_teacher(self, phone, name, identity, pwd):
+        """添加老师"""
+        data = {
+            "phone": phone,
+            "name": name,
+            "identity": identity,
+            "pwd": pwd
+        }
+        path = "backend/user/add-teacher"
+        return self.send("post", path, json=data)
+
+    def disable_user(self, uuid):
+        """禁用用户"""
+        data = {
+            "uuid": uuid
+        }
+        path = "backend/user/disable"
+        return self.send("post", path, json=data)
+
+    def import_teacher(self):
+        """导入老师"""
+        path = "backend/user/import-teacher"
+        files = {
+            "upload-file": ("老师模版.xlsx",
+                            open(r"E:\python_project\own_project\web_ui\sass_apiauto\files\老师模版.xlsx", "rb"),
+                            # open(r"files/dog.jpeg", "rb"),
+                            "application/octet-stream"
+                            )
+        }
+        return self.send("post", path, files=files)
+
+    def import_student(self):
+        """导入学生"""
+        path = "backend/user/import-student"
+        files = {
+            "upload-file": ("老师模版.xlsx",
+                            open(r"E:\python_project\own_project\web_ui\sass_apiauto\files\学生模版.xlsx", "rb"),
+                            # open(r"files/dog.jpeg", "rb"),
+                            "application/octet-stream"
+                            )
+        }
+        return self.send("post", path, files=files)
+
+    def edit_user(self, uuid, nickname, phone, real_name, identities, gender, stu_no):
+        """编辑用户"""
+        path = "backend/user/modify-user"
+        data = {
+            "uuid": uuid,
+            "nickname": nickname,
+            "phone": phone,
+            "real_name": real_name,
+            "identities": identities,
+            "gender": gender,
+            "stu_no": stu_no
+        }
+        return self.send("post", path, json=data)
+
+    def user_detail(self, uuid):
+        """用户信息"""
+        path = "backend/user/user-detail"
+        data = {
+            "uuid": uuid
+        }
+        return self.send("get", path, params=data)
+
+    def add_user(self, phone, name, identities, pwd):
+        """添加用户"""
+        path = "backend/user/add-user"
+        data = {
+            "phone": phone,
+            "name": name,
+            "identities": identities,
+            "pwd": pwd
+        }
+        return self.send("post", path, json=data)
+
+    def unblock_users(self, uuid):
+        """解封用户"""
+        path = "backend/user/open"
+        data = {
+            "uuid": uuid
+        }
+        return self.send("post", path, json=data)
+
+    def switching_identities(self, role_id):
+        """切换角色"""
+        path = "v2/user/switch-role"
+        data = {
+            "role_id": role_id
+        }
+        return self.send("post", path, json=data)
+
+    def user_info(self):
+        """用户接口信息"""
+        path = "backend/user/info"
+        return self.send("get", path)
+
--- testcases/test_user_manage.py
+++ testcases/test_user_manage.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import allure
 import pytest
-
 from api.BaseApi import BaseApi
 from api.UserManageApi import UserManageApi
 from testcases.conftest import get_data
+from utils.jsonpath_utils import JsonPathUtils
 
 
 @allure.feature("用户管理模块")
@@ -18,4 +18,285 @@ class TestUserManageApi(BaseApi):
     @allure.description("获取用户列表")
     @pytest.mark.parametrize("data", get_data()['get_user_list'])
     def test_get_user_list(self, data):
-        self.user_manage.get_user_list(data["page"], data["size"])
+        r = self.user_manage.get_user_list(data["page"], data["size"])
+        assert r.get("msg") == "success"
+
+    @allure.story("添加学生测试用例")
+    @allure.title("添加学生")
+    @allure.severity("normal")
+    @allure.description("添加学生")
+    @pytest.mark.parametrize("data", get_data()["add_student"])
+    def test_add_student(self, data):
+        self.user_manage.add_student(data["phone"], data["names"], data["pwd"])
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("添加学生输入异常手机号测试用例")
+    @allure.title("添加学生输入异常手机号")
+    @allure.severity("normal")
+    @allure.description("添加学生输入异常手机号")
+    @pytest.mark.parametrize("data", get_data()["add_student_phone"])
+    def test_add_student_phone(self, data):
+        r = self.user_manage.add_student(data["phone"], data["names"], data["pwd"])
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 51
+        assert len(code) == 1 and code[0] == 51
+
+    @allure.story("添加老师测试用例")
+    @allure.title("添加老师")
+    @allure.severity("normal")
+    @allure.description("添加老师")
+    @pytest.mark.parametrize("data", get_data()["add_teacher"])
+    def test_add_teacher(self, data):
+        self.user_manage.add_teacher(data["phone"], data["names"], data["identity"], data["pwd"])
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("禁用用户测试用例")
+    @allure.title("禁用用户")
+    @allure.severity("normal")
+    @allure.description("禁用用户")
+    @pytest.mark.parametrize("data", get_data()["add_teacher"])
+    def test_disable_user(self, data):
+        self.user_manage.add_teacher(data["phone"], data["names"], data["identity"], data["pwd"])
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        self.user_manage.disable_user(user_uuid)
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("导入老师测试用例")
+    @allure.title("导入老师")
+    @allure.severity("normal")
+    @allure.description("导入老师")
+    def test_import_teacher(self):
+        self.user_manage.import_teacher()
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("导入已存在老师测试用例")
+    @allure.title("导入已存在老师")
+    @allure.severity("normal")
+    @allure.description("导入已存在老师")
+    def test_import_teacher_double(self):
+        self.user_manage.import_teacher()
+        r = self.user_manage.import_teacher()
+        err_count = JsonPathUtils.get(r, "$..err_count")
+        assert len(err_count) == 1 and err_count[0] == 1
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("导入学生测试用例")
+    @allure.title("导入学生")
+    @allure.severity("normal")
+    @allure.description("导入学生")
+    def test_import_student(self):
+        self.user_manage.import_student()
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("导入已存在学生测试用例")
+    @allure.title("导入已存在学生")
+    @allure.severity("normal")
+    @allure.description("导入已存在学生")
+    def test_import_student_double(self):
+        self.user_manage.import_student()
+        r = self.user_manage.import_student()
+        err_count = JsonPathUtils.get(r, "$..err_count")
+        assert len(err_count) == 1 and err_count[0] == 1
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("编辑用户测试用例")
+    @allure.title("编辑用户")
+    @allure.severity("normal")
+    @allure.description("编辑用户")
+    @pytest.mark.parametrize("data", get_data()["edit_user"])
+    def test_edit_user(self, data):
+        self.user_manage.import_student()
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        self.user_manage.edit_user(user_uuid, data["nickname"], data["phone"], data["real_name"], data["identities"],
+                                   data["gender"], data["stu_no"])
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("编辑用户手机号为异常号码测试用例")
+    @allure.title("编辑用户手机号为异常号码")
+    @allure.severity("normal")
+    @allure.description("编辑用户手机号为异常号码")
+    @pytest.mark.parametrize("data", get_data()["edit_user_phone"])
+    def test_edit_user(self, data):
+        self.user_manage.import_student()
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        self.user_manage.edit_user(user_uuid, data["nickname"], data["phone"], data["real_name"], data["identities"],
+                                   data["gender"], data["stu_no"])
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("编辑用户角色为不存在角色测试用例")
+    @allure.title("编辑用户角色为不存在角色")
+    @allure.severity("normal")
+    @allure.description("编辑用户角色为不存在角色")
+    @pytest.mark.parametrize("data", get_data()["edit_user_identities"])
+    def test_edit_user(self, data):
+        self.user_manage.import_student()
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.edit_user(user_uuid, data["nickname"], data["phone"], data["real_name"],
+                                       data["identities"],
+                                       data["gender"], data["stu_no"])
+        msg = JsonPathUtils.get(r, "$..msg")[0]
+        assert msg == "角色不存在"
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("获取用户信息测试用例")
+    @allure.title("获取用户信息")
+    @allure.severity("normal")
+    @allure.description("获取用户信息")
+    def test_user_detail(self):
+        self.user_manage.import_student()
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        self.user_manage.user_detail(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")[0]
+        assert code == 0
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("添加用户测试用例")
+    @allure.title("添加用户")
+    @allure.severity("normal")
+    @allure.description("添加用户")
+    @pytest.mark.parametrize("data", get_data()["add_user"])
+    def test_add_user(self, data):
+        r = self.user_manage.add_user(data["phone"], data["names"], data["identities"], data["pwd"])
+        assert r.get("code") == 0
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("解禁用户测试用例")
+    @allure.title("解禁用户")
+    @allure.severity("normal")
+    @allure.description("解禁用户")
+    @pytest.mark.parametrize("data", get_data()["add_user"])
+    def test_unblock_user(self, data):
+        r = self.user_manage.add_user(data["phone"], data["names"], data["identities"], data["pwd"])
+        assert r.get("code") == 0
+        r = self.user_manage.get_user_list_null()
+        page = JsonPathUtils.get(r, "$..pagination.pages")
+        size = JsonPathUtils.get(r, "$..pagination.size")
+        r = self.user_manage.get_user_list(page, size)
+        user_uuid = JsonPathUtils.get(r, "$..list[(@.length-1)].uuid")[0]
+        r = self.user_manage.disable_user(user_uuid)
+        assert r.get("code") == 0
+        r = self.user_manage.unblock_users(user_uuid)
+        assert r.get("code") == 0
+        r = self.user_manage.del_user(user_uuid)
+        code = JsonPathUtils.get(r, "$..code")
+        # 断言
+        assert r.get("code") == 0
+        assert len(code) == 1 and code[0] == 0
+
+    @allure.story("用户信息接口测试用例")
+    @allure.title("用户信息接口")
+    @allure.severity("normal")
+    @allure.description("用户信息接口")
+    def test_switching_identities_teacher(self):
+        r = self.user_manage.user_info()
+        assert r.get("code") == 0
+
+
+
+
+
+
